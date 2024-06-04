af1d7706c33a2cfdcaf8791eedba2380a1abd70c
B.P.S <81668353+BPS-sys@users.noreply.github.com>
2024-06-04 17:38:42 +0900

    Add files via upload
    辞書から学習用モデルを作成できる実行ファイル

---
+++ create_trainpyfile.py
@@ -0,0 +1,158 @@
+
+# 2024/06/04 BPS-sys ver1.0
+
+
+class ModelInfo:
+    """
+    Use dict to create model structure.
+    """
+    def __init__(self):
+        self.layers = ''
+        self.imports = ''
+        self.load_import_info()
+    
+    def load_import_info(self):
+        """
+        read import file.
+        """
+        with open('imports_file.txt') as f:
+            imports_data = f.read()
+        self.imports += imports_data + '\n\n'
+
+    def send_model(self, model_dict):
+        """
+        Please model structure dict.
+        """
+        before_unique_layer_name = ''    # １つ前のレイヤー変数名(layername)
+        first_unique_layer_name = ''     # 最初のレイヤー変数名(inputs)
+        filal_unique_layer_name = ''     # 最後のレイヤー変数名(outputs)
+        for i, (unique_layer_name, layer_params) in enumerate(model_dict.items()):
+            params = ''    # 各レイヤーパラメータの格納用変数
+            if i == 0:
+                first_unique_layer_name = unique_layer_name
+            if i == len(model_dict)-1:
+                filal_unique_layer_name = unique_layer_name
+            
+            # レイヤー関数名を取得
+            layer_name = unique_layer_name[:-4]
+
+            # パラメータ引数をセット
+            for layer_param_name, layer_param_value in layer_params.items():
+                params += f'{layer_param_name}={layer_param_value},'
+            
+            # 不要なコンマを削除
+            params = params[:-1]
+
+            # 行ごとにレイヤー作成
+            if before_unique_layer_name:
+                self.layers += f'    {unique_layer_name} = {layer_name}({params})({before_unique_layer_name})\n'
+            else:
+                self.layers += f'    {unique_layer_name} = {layer_name}({params})\n'
+            
+            # １つ前のレイヤー名を更新
+            before_unique_layer_name = unique_layer_name
+            
+        # 最終層作成
+        self.layers += f'    model = Model(inputs={first_unique_layer_name}, outputs={filal_unique_layer_name})\n'
+        
+    def write_modelfile(self):
+        """
+        create model_info.py file.
+        """
+        if self.layers:
+            with open('model_info.py', 'w') as f:
+                f.write(self.imports+'def model_build():\n'+self.layers+'    return model')
+            
+
+if __name__ == '__main__':
+    # テストケース
+    test_dic = {
+        'Dense0000': {
+            'units': 0,
+            'use_bias': True,
+            'kernel_initializer': '\'glorot_uniform\'',
+            'bias_initializer': '\'zeros\'',
+            'kernel_regularizer': None,
+            'bias_regularizer': None,
+            'activity_regularizer': None,
+            'kernel_constraint': None,
+            'bias_constraint': None,
+            'lora_rank': None
+        },
+
+        'Activation0000': {
+            'activation':None
+        },
+
+        'Conv2D0000': {
+            'filters': 0,
+            'kernel_size': (0, 0),
+            'strides': (1, 1),
+            'padding': '\'valid\'',
+            'data_format': None,
+            'dilation_rate': (1, 1),
+            'groups': 1,
+            'activation': None,
+            'use_bias': True,
+            'kernel_initializer': '\'glorot_uniform\'',
+            'bias_initializer': '\'zeros\'',
+            'kernel_regularizer': None,
+            'bias_regularizer': None,
+            'activity_regularizer': None,
+            'kernel_constraint': None,
+            'bias_constraint': None
+        },
+
+        'Conv1D0000': {
+            'filters': 0,
+            'kernel_size': 0,
+            'strides': 1,
+            'padding': '\'valid\'',
+            'data_format': None,
+            'dilation_rate': 1,
+            'groups': 1,
+            'activation': None,
+            'use_bias': True,
+            'kernel_initializer': '\'glorot_uniform\'',
+            'bias_initializer': '\'zeros\'',
+            'kernel_regularizer': None,
+            'bias_regularizer': None,
+            'activity_regularizer': None,
+            'kernel_constraint': None,
+            'bias_constraint': None
+        },
+
+        'MaxPooling2D0000': {
+            'pool_size': (2, 2),
+            'strides': None,
+            'padding': '\'valid\'',
+            'data_format': None,
+            'name': None
+        },
+
+        'MaxPooling1D0000': {
+            'pool_size': 2,
+            'strides': None,
+            'padding': '\'valid\'',
+            'data_format': None,
+            'name': None
+        },
+
+        'Flatten0000': {
+            'data_format': None
+        },
+
+        'GlobalAveragePooling2D0000': {
+            'data_format': None,
+            'keepdims': False
+        },
+
+
+    }
+
+
+    # 実行
+
+    model_info = ModelInfo()
+    model_info.send_model(test_dic)
+    model_info.write_modelfile()
