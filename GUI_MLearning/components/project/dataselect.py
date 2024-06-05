f6576c31948b8605b79413cf83348c7556e3aa39
paccyo <138096361+paccyo@users.noreply.github.com>
2024-06-06 02:49:58 +0900

    Delete GUI_MLearning directory

--- GUI_MLearning/components/project/dataselect.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-class DataSelect(ft.Tab):
-    def __init__(self, page:ft.Page, text: str | None = None, content: ft.Control | None = None, tab_content: ft.Control | None = None, icon: str | None = None, ref: ft.Ref | None = None, visible: bool | None = None, adaptive: bool | None = None):
-        super().__init__(text, content, tab_content, icon, ref, visible, adaptive)
-        self.page = page
-        self.icon=ft.icons.DATA_USAGE
-        self.content=ft.Text("data selection")
