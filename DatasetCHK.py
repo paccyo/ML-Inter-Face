6a82e2c79897196b75b723ba16b38f570e307534
B.P.S <81668353+BPS-sys@users.noreply.github.com>
2024-06-09 05:13:08 +0900

    Add files via upload

--- DatasetCHK.py
+++ DatasetCHK.py
@@ -2,8 +2,10 @@
 import os
 import glob
 from mimetypes import guess_type
+import pandas as pd
 
 """
+image:
 dataset----train------cat---~~.png
         |          |--dog---~~.png
         | 
@@ -12,7 +14,7 @@ dataset----train------cat---~~.png
 """
 
 
-def CHK(path=None, data_type=None, learning_way=None):
+def CHK(path=None, data_type=None, learning_way=None, target=None):
     """
     データの型が正しいかチェックします。
     path:データのパス
@@ -20,19 +22,44 @@ def CHK(path=None, data_type=None, learning_way=None):
     learning_way:学習方法
     """
     break_flag = False
-    if data_type == 'image' and learning_way == 'categorical':
-        datasets_path = path
-        for label_name in glob.glob(os.path.join(datasets_path, '*')):
-            for filename in glob.glob(os.path.join(os.path.join(datasets_path, label_name), '*.*')):
-                guess = guess_type(os.path.basename(filename))
-                if 'image' not in str(guess):
-                    break_flag = True
-                    break
-            if break_flag:
-                return False
-        return True
+    datasets_path = path
+    results = []
+    if learning_way == 'categorical':
+        if data_type == 'image':
+            label_num = 0
+            for label_name in glob.glob(os.path.join(datasets_path, '*')):
+                label_num += 1
+                for filename in glob.glob(os.path.join(os.path.join(datasets_path, label_name), '*.*')):
+                    guess = guess_type(os.path.basename(filename))
+                    if data_type in str(guess):
+                        image_path = os.path.join(os.path.join(datasets_path, label_name), filename)
+                        results.append(image_path)
+                    else:
+                        break_flag = True
+                        break
+                if break_flag:
+                    return False, []
+            if label_num >= 2:
+                return True, results
+            else:
+                return False, []
+
+        elif data_type == 'text':
+            if '.csv' == os.path.splitext(os.path.basename(datasets_path))[-1]:
+                df = pd.read_csv(datasets_path)
+                try:
+                    target_df = df[target]
+                    df.pop(target)
+                    results.append([df, target_df])
+                except KeyError:
+                    return False, []
+                else:
+                    return True, results
+            else:
+                return False, []
             
 
+
 if __name__ == '__main__':
-    judge = CHK(path=None, data_type='image', learning_way='categorical')
+    judge = CHK(path='data.csv', data_type='text', learning_way='categorical', target='data')
     print(judge)
