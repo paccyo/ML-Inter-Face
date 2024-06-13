3571b411c245a54c2e83670e2f32f0a840d1b79a
B.P.S <81668353+BPS-sys@users.noreply.github.com>
2024-06-13 19:22:36 +0900

    Add files via upload

--- GetShape.py
+++ GetShape.py
@@ -1,12 +1,13 @@
 
-from PIL import Image
 import numpy as np
 
-def get(path):
-    image = Image.open(path)
-    array = np.array(image)
-    return array.shape
+def get(image_size=(None, None), color_mode='rgb'):    
+    if color_mode == 'rgb':
+        color = 3
+    else:
+        color = 1
+    return (image_size[0], image_size[1], color)
 
 if __name__ == '__main__':
-    shape_size = get(r"path")
+    shape_size = get(image_size=(256, 256), color_mode='rgb')
     print(shape_size)
