cfade7eb3f7351e93664c7a0105cd36878e8c706
B.P.S <81668353+BPS-sys@users.noreply.github.com>
2024-06-15 19:38:52 +0900

    Update output_model_graph.py

--- GUI_MLearning/packages/output_model_graph.py
+++ GUI_MLearning/packages/output_model_graph.py
@@ -3,6 +3,6 @@ from keras.utils import plot_model
 import sys
 
 
-project_path = sys.argv[0]
+_, project_path = sys.argv[0], sys.argv[1]
 model = model_info.model_build(project_path)
 plot_model(model, show_shapes=True, expand_nested=True, to_file=f'{project_path}/model.png')
