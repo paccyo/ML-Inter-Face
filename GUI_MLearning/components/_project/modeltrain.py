fa79eb69badbcbbfcc9f337f18c323b23501e0e4
paccyo <138096361+paccyo@users.noreply.github.com>
2024-06-17 00:07:17 +0900

    Update modeltrain.py

--- GUI_MLearning/components/_project/modeltrain.py
+++ GUI_MLearning/components/_project/modeltrain.py
@@ -7,6 +7,7 @@ import flet.canvas as cv
 import glob
 import asyncio
 import shutil
+import os
 
 class ModelTrain(ft.Tab):
     def __init__(self, page:ft.Page, text: str | None = None, content: ft.Control | None = None, tab_content: ft.Control | None = None, icon: str | None = None, ref: ft.Ref | None = None, visible: bool | None = None, adaptive: bool | None = None):
