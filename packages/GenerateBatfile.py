70fc59715d8506c944584135226a585b764803e5
DESKTOP-FL59MJ2\sakiya <zaq1122s@gmail.com>
2024-06-21 23:48:03 +0900

    GenerateBatの更新 pyファイル呼び出しの際のpythonを指定

--- packages/GenerateBatfile.py
+++ packages/GenerateBatfile.py
@@ -12,7 +12,7 @@ def generate(target_path, run_path, part_dict, data_type, epochs, batchs=None, p
     test_part = part_dict['test']
     if data_type == 'image':
         with open(target_path+'/run.bat', 'w') as f:
-            f.write(f'call "{run_path}"\n{target_path}/RunTrain.py {train_part} {validatioin_part} {test_part} {data_type} {epochs} {batchs} {project_path}')
+            f.write(f'call "{run_path}"\npython {target_path}/RunTrain.py {train_part} {validatioin_part} {test_part} {data_type} {epochs} {batchs} {project_path}')
         # f.write(f'echo trainstart\npause\n{run_path}\npause\nstart {target_path}/RunTrain.py\npause\necho fin\npause')
 
 def Runbat(batfile_path):
@@ -32,5 +32,5 @@ def generate_output_graph_bat(target_path, run_path, project_path):
     project_path->str: "プロジェクト名/Result"絶対パス
     """
     with open(target_path+'/output_model_graph_run.bat', 'w') as f:
-            f.write(f'call "{run_path}"\n{target_path}/output_model_graph.py {project_path}')
+            f.write(f'call "{run_path}"\npython {target_path}/output_model_graph.py {project_path}')
 
