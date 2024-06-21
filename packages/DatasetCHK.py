e5ade09f4654871047c6975dc3449299e3e69fc6
B.P.S <prgrmsilver@gmail.com>
2024-06-22 05:09:01 +0900

    dataframeのデータセット型チェック追加 一部ファイルの場所を移動

--- packages/DatasetCHK.py
+++ packages/DatasetCHK.py
@@ -22,39 +22,36 @@ def CHK(path=None, data_type=None, learning_way=None, target=None):
     learning_way:学習方法
     """
     datasets_path = path
-    results = []
+    results = {}
     if learning_way == 'categorical':
         if data_type == 'image':
             label_num = 0
             for label_path in glob.glob(os.path.join(datasets_path, '*')):
+                result_image_c = 0
                 label_num += 1
+                temp = []
                 if not os.path.isdir(label_path):
-                    return False, []
+                    return False, {}
                 for file_path in glob.glob(os.path.join(label_path, '*.*')):
                     guess = guess_type(os.path.basename(file_path))
-                    if data_type in str(guess):
-                        results.append(file_path)
+                    if data_type in str(guess) and result_image_c < 10:
+                        result_image_c += 1
+                        temp.append(file_path)
                     else:
-                        return False, []
+                        return False, {}
+                label_name = label_path.replace('\\', '/').split('/')[-1]
+                results[label_name] = temp
             
             if label_num >= 2:
                 return True, results
             else:
-                return False, []
+                return False, {}
 
-        elif data_type == 'text':
+        elif data_type == 'dataframe':
             if '.csv' == os.path.splitext(os.path.basename(datasets_path))[-1]:
-                df = pd.read_csv(datasets_path)
-                try:
-                    target_df = df[target]
-                    df.pop(target)
-                    results.append([df, target_df])
-                except KeyError:
-                    return False, []
-                else:
-                    return True, results
+                return True, {}
             else:
-                return False, []
+                return False, {}
             
 
 
