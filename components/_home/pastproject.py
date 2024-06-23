8fd587828599bf960e78dd99918ed4820b0728ef
DESKTOP-FL59MJ2\sakiya <zaq1122s@gmail.com>
2024-06-23 23:38:07 +0900

    pastprojectの更新

--- components/_home/pastproject.py
+++ components/_home/pastproject.py
@@ -11,10 +11,8 @@ class PastProject(ft.Container):
         self.left=self.page.width//7
         self.height=(self.page.height//5)*3
         self.width=(self.page.width//7)*5
-
-
+        # self.alignment=ft.alignment.center
         self.pastproject_list = glob.glob('projects/*')
-
         past_project = []
         for project in self.pastproject_list:
             rect = []
@@ -30,9 +28,7 @@ class PastProject(ft.Container):
                     # horizontal_alignment=ft.MainAxisAlignment.START,
                     spacing=5,
                 ),
-                style=ft.ButtonStyle(
-                    shape=ft.RoundedRectangleBorder(radius=0),
-                ),
+                style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)),
                 on_click=self.on_click_past_project,
                 width = self.width,
             )
@@ -41,14 +37,14 @@ class PastProject(ft.Container):
             if rect != []:
                 past_project.append(rect)
             
-        
         # print(self.pastproject_list)
         self.past_project = ft.Container(
             content=ft.Column(
                 controls=past_project,
                 scroll=ft.ScrollMode.HIDDEN,
             ),
-            height = self.height-50
+            height = self.height-50,
+            alignment=ft.alignment.top_center,
         )
         
         
