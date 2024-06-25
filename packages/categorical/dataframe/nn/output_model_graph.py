da17b74b266e8a23ca771fd65fd925628f5d2c35
BPS-sys <prgrmsilver@gmail.com>
2024-06-25 23:33:12 +0900

    dataframe分類用NNの追加

---
+++ packages/categorical/dataframe/nn/output_model_graph.py
@@ -0,0 +1,9 @@
+import model_info
+from keras.utils import plot_model
+import sys
+
+
+_, project_path = sys.argv[0], sys.argv[1]
+model = model_info.model_build()
+
+plot_model(model, show_shapes=True, expand_nested=True, to_file=f'{project_path}/model.png')
