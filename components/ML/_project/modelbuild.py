e2fa982e563e3e8f322339b279a5885206dcf47b
paccyo <YUxtukuri1122s>
2024-06-28 12:46:28 +0900

    no message

--- components/ML/_project/modelbuild.py
+++ components/ML/_project/modelbuild.py
@@ -8,8 +8,8 @@ import flet.canvas as cv
 import copy
 
 class ModelBuild(ft.Tab):
-    def __init__(self, page: ft.Page, text: str | None = None, content: ft.Control | None = None, tab_content: ft.Control | None = None, icon: str | None = None, ref: ft.Ref | None = None, visible: bool | None = None, adaptive: bool | None = None):
-        super().__init__(text, content, tab_content, icon, ref, visible, adaptive)
+    def __init__(self, page: ft.Page):
+        super().__init__()
         self.page = page
         self.text="モデル構築"
         self.icon=ft.icons.NUMBERS
