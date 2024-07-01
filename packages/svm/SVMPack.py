7c9cced74a51b3bd526e424527939e4b207d4eed
BPS-sys <prgrmsilver@gmail.com>
2024-07-01 10:59:45 +0900

    ROC曲線プロット機能の追加

--- packages/svm/SVMPack.py
+++ packages/svm/SVMPack.py
@@ -1,6 +1,10 @@
 
 import pandas as pd
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 
 
 class SVMToolKit:
@@ -21,19 +25,51 @@ class SVMToolKit:
         self.train_df_target = pd.read_csv(f'{self.dataset_path}/train_target.csv')
         self.validation_df_target = pd.read_csv(f'{self.dataset_path}/validation_target.csv')
         self.test_df_target = pd.read_csv(f'{self.dataset_path}/test_target.csv')
+        # データセット作成
+        self.train_data = self.train_df_data.values
+        self.validation_data = self.validation_df_data.values
+        self.test_data = self.test_df_data.values
+        self.train_target = self.train_df_target.values
+        self.validation_target = self.validation_df_target.values
+        self.test_target = self.test_df_target.values
 
     def set_params(self):
-        self.svm = SVC()
+        self.svm = SVC(probability=True)
 
     def train(self):
-        self.svm.fit(self.train_df_data.values, self.train_df_target.values.ravel())
+        self.svm.fit(self.train_data, self.train_target.ravel())
+
+    def evaluate(self, mode='validation'):
+        if mode == 'validation':
+            pred = self.svm.predict(self.validation_data)
+            print('accuracy：', accuracy_score(y_true=self.validation_target, y_pred=pred))
+            print('precision：', precision_score(y_true=self.validation_target, y_pred=pred, average='macro'))
+            print('recall：', recall_score(y_true=self.validation_target, y_pred=pred, average='macro'))
+            print('f1 score：', f1_score(y_true=self.validation_target, y_pred=pred, average='macro'))
+            print('confusion matrix = \n', confusion_matrix(y_true=self.validation_target, y_pred=pred))
+            #ROC曲線の描画、AUCの計算（ROC曲線の下側の面積）の計算
+            n_classes = len(self.svm.classes_)
+            classes = self.svm.classes_
+            y_test_one_hot = label_binarize(self.validation_target, classes=classes)
+            fpr = {}
+            tpr = {}
+            roc_auc = {}
+            pred_proba = self.svm.predict_proba(self.validation_data)
+            for i in range(n_classes):
+                fpr[i], tpr[i], _ = roc_curve(y_test_one_hot[:, i], pred_proba[:, i])
+                roc_auc[i] = auc(fpr[i], tpr[i])
+            for i, class_ in enumerate(classes):
+                plt.plot(fpr[i], tpr[i], label=f'{class_}')
+            plt.legend()   
+            plt.show()
+
 
     def save_model_fig(self):
         pass
 
 if __name__ == '__main__':
-    lrkit = SVMToolKit()
+    lrkit = SVMToolKit(r'test_data', None)
     lrkit.load_dataset()
     lrkit.set_params()
     lrkit.train()
-    lrkit.save_model_fig()
+    lrkit.evaluate()
