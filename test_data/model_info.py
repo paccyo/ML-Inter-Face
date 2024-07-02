e746055ff8bd389ef72ff80c6291523f1de037d7
BPS-sys <prgrmsilver@gmail.com>
2024-07-02 14:11:20 +0900

    テスト用データセット更新

---
+++ test_data/model_info.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.svm import SVC
+
+def model_build():
+    model = DecisionTreeRegressor(alg=DecisionTreeRegressor, max_depth=5)
+    return model
