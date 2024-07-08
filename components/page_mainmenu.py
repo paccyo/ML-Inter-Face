52c36b0f2548530d5a15a6bc2cfff6eb0dd924a5
Taiga Kadowaki <taitai73@outlook.jp>
2024-07-08 10:40:48 +0900

    メインメニューのボタン配置等の変更

--- components/page_mainmenu.py
+++ components/page_mainmenu.py
@@ -2,18 +2,47 @@ from components._common.appheader import AppHeader
 import flet as ft
 
 class MainMenu(ft.View):
-    def __init__(self, page:ft.Page):
+    def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
         self.route = "/Page_MainMenu"
         self.controls = [
-            AppHeader(self.page,title="ML/DS InterFace",bgcolor=ft.colors.LIME),
-            ft.ElevatedButton(text="ML",on_click=self.on_click_ML),
-            ft.ElevatedButton(text="DS",on_click=self.on_click_DS)
+            AppHeader(self.page, title="ML/DS InterFace", bgcolor=ft.colors.LIME),
+            ft.Container(
+                content=ft.Row(
+                    controls=[
+                        ft.ElevatedButton(
+                            text="ML",
+                            on_click=self.on_click_ML,
+                            width=200,
+                            height=100,
+                            bgcolor=ft.colors.BLUE,
+                            style=ft.ButtonStyle(
+                                color=ft.colors.WHITE,
+                            )
+                        ),
+                        ft.ElevatedButton(
+                            text="DS",
+                            on_click=self.on_click_DS,
+                            width=200,
+                            height=100,
+                            bgcolor=ft.colors.ORANGE,
+                            style=ft.ButtonStyle(
+                                color=ft.colors.WHITE,
+                            )
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=20,
+                ),
+                alignment=ft.alignment.center,
+                expand=True
+            )
         ]
-    
-    def on_click_ML(self,e):
+
+    def on_click_ML(self, e):
         self.page.go("/Page_MLHome")
-    
-    def on_click_DS(self,e):
+
+    def on_click_DS(self, e):
         self.page.go("/Page_DSHome")
