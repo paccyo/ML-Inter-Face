b70ac32880cde57c2d98fc5d362b65d1a837b996
BPS-sys <prgrmsilver@gmail.com>
2024-07-20 23:43:18 +0900

    データフレームからデータ分析できるように変更

--- packages/DS.py
+++ packages/DS.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from autoviz.AutoViz_Class import AutoViz_Class
 import matplotlib.pyplot as plt
+from io import StringIO
 
 
 class DS:
@@ -9,35 +10,29 @@ class DS:
     def __init__(self):
         self.df = None
         self.file_name = 'DS_data.csv'
-        self.file_path = None
-
-    def send(self, data, export_dir_data, export_dir_result):
-        self.load_dataframe(data, export_dir_data)
+        
+    def send(self, data, export_dir_result):
+        self.load_dataframe(data)
         self.analyze(export_dir_result)
 
-    def load_dataframe(self, data, export_dir):
+    def load_dataframe(self, data):
         if type(data) == str:
             self.df = pd.read_csv(data)
         elif type(data) == pd.DataFrame:
             self.df = data
-        self.file_path = f'{export_dir}/{self.file_name}'
-        self.df_to_csv()
-
-    def df_to_csv(self):
-        self.df.to_csv(self.file_path)
         
     def analyze(self, export_dir):
         # AutoVizのインスタンスを作成
         AV = AutoViz_Class()
         # AutoVizを使ってデータを可視化
+        print(self.df)
         dft = AV.AutoViz(
-            self.file_path,
+            filename=None,
+            dfte=self.df,
             depVar="",  # 目的変数名（必要に応じて変更）
-            dfte=None,
             header=0,
-            sep=",",
-            verbose=0,
-            lowess=False,
+            verbose=1,
+            lowess=True,
             chart_format="html",
             save_plot_dir=f'{export_dir}/DS_result'
         )
@@ -45,6 +40,5 @@ class DS:
 
 if __name__ == '__main__':
     ds = DS()
-    ds.send(data=r"C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\test_data\data.csv",
-            export_dir_data=r"C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\test_data\test_dir",
+    ds.send(data=r"C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\test_data\train_data.csv",
             export_dir_result=r"C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\test_data\test_dir")
