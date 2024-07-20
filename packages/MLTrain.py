25c10a8f52d3c5621eed5dde41940a95a7080049
BPS-sys <prgrmsilver@gmail.com>
2024-07-20 23:46:52 +0900

    学習不可のバグ修正

--- packages/MLTrain.py
+++ packages/MLTrain.py
@@ -1,8 +1,10 @@
 
 import pandas as pd
 from packages import evaluate
+from packages import convert_type
 import model_info
 import sys
+import numpy as np
 
 
 class TrainToolKit:
@@ -26,32 +28,33 @@ class TrainToolKit:
             self.train_df_data = pd.read_csv(f'{self.dataset_path}/train_data.csv')
             self.train_df_target = pd.read_csv(f'{self.dataset_path}/train_target.csv')
             self.train_data = self.train_df_data.values
-            self.train_target = self.train_df_target.values
+            self.train_target = np.array(self.train_df_target.values)
         
         if self.validation_part != 0:
             self.validation_df_data = pd.read_csv(f'{self.dataset_path}/validation_data.csv')
             self.validation_df_target = pd.read_csv(f'{self.dataset_path}/validation_target.csv')
             self.validation_data = self.validation_df_data.values
-            self.validation_target = self.validation_df_target.values
+            self.validation_target = np.array(self.validation_df_target.values)
 
         if self.test_part != 0:
             self.test_df_data = pd.read_csv(f'{self.dataset_path}/test_data.csv')    
             self.test_df_target = pd.read_csv(f'{self.dataset_path}/test_target.csv')
             self.test_data = self.test_df_data.values
-            self.test_target = self.test_df_target.values
-
+            self.test_target = np.array(self.test_df_target.values)
+            
     def load_model(self):
-        self.model = model_info.build_model()
+        self.model = model_info.model_build()
         
     def train(self):
         self.model.fit(self.train_data, self.train_target.ravel())
 
 
-_, dataset_path, export_path, train_part, validation_part, test_part, train_mode, alg  = sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]
+# _, dataset_path, export_path, train_part, validation_part, test_part, train_mode, alg  = sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]
+_, dataset_path, export_path, train_part, validation_part, test_part, train_mode, alg  = None, r'test_data/dataset', r"test_data/test_dir", 6, 4, 0, 'classifier', 'decisiontree'
 # train_mode = classifier or reg
 # alg = decisiontree, randomforest, SVM
 train_part, validation_part, test_part, int(train_part), int(validation_part), int(test_part)
-kit = TrainToolKit(r'test_data/dataset', None, {'train':train_part, 'validation':validation_part, 'test':test_part})
+kit = TrainToolKit(dataset_path, None, {'train':train_part, 'validation':validation_part, 'test':test_part})
 kit.load_dataset()
 kit.load_model()
 kit.train()
