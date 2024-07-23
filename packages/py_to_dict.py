0dc94598b1d3e552aea923a85fb7148cddbf74fd
BPS-sys <prgrmsilver@gmail.com>
2024-07-23 21:01:54 +0900

    model_info.pyをdict型に変換します。

--- packages/py_to_dict.py
+++ packages/py_to_dict.py
@@ -1,54 +1,81 @@
 
-from util import Calldict
+from packages.util import Calldict
 import re
 
-with open(r'C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\model_info_NN.py') as f:
-    code_lines = f.readlines()
-original_dic = Calldict.layer_dicts
-deal_start = False
-first = True
-for i, code_line in enumerate(code_lines):
-    if 'def model_build():' in code_line:
-        deal_start = True
-        continue
-    elif 'return' in code_line:
-        continue
-    elif 'Model' in code_line:
-        continue
-    if not deal_start:
-        continue
-    code_line = code_line.replace(' ', '').replace('\n', '')
-    layer_name = code_line.split('=')[1].split('(')[0]
 
-    start_index = None
-    end_index = None
-    c = 0
-    for j, letter in enumerate(code_line):
-        if letter == '(':
-            start_index = j+1
-            break
-    for j, letter in enumerate(code_line[::-1]):
-        if letter == ')':
-            c += 1
-            if c == 2 or first:
-                end_index = -(j+1)
-                first = False
+def convert_model_to_dict(path):
+    """
+    model_info.pyをdictに変換します。
+    """
+    with open(path) as f:
+        code_lines = f.readlines()
+    original_layer_dic = Calldict.layer_dicts
+    deal_start = False
+    first = True
+    result = []
+    temp_result = {}
+    for i, code_line in enumerate(code_lines):
+        if 'def model_build():' in code_line:
+            deal_start = True
+            continue
+        elif 'return' in code_line:
+            continue
+        elif 'Model' in code_line:
+            continue
+        if not deal_start:
+            continue
+        code_line = code_line.replace(' ', '').replace('\n', '')
+        # レイヤ名
+        layer_name = code_line.split('=')[1].split('(')[0]
+
+        start_index = None
+        end_index = None
+        c = 0
+        for j, letter in enumerate(code_line):
+            if letter == '(':
+                start_index = j+1
                 break
-    text = code_line[start_index:end_index]
-    pattern = r'\(([^)]+)\)'
-    pattern_list = []
-    # パターンに一致する部分をすべて見つける
-    matches = re.findall(pattern, text)
-    for m in matches:
-        pattern_list.append('(' + m + ')')
-    for j, pat in enumerate(pattern_list):
-        text = text.replace(pat, f'#{j}')
-    text_list = text.split(',')
-    print(text_list)
-    
+        for j, letter in enumerate(code_line[::-1]):
+            if letter == ')':
+                c += 1
+                if c == 2 or first:
+                    end_index = -(j+1)
+                    first = False
+                    break
+        text = code_line[start_index:end_index]
+        pattern = r'\(([^)]+)\)'
+        pattern_list = []
+        # パターンに一致する部分をすべて見つける
+        matches = re.findall(pattern, text)
+        for m in matches:
+            pattern_list.append(m)
+        for j, pat in enumerate(pattern_list):
+            text = text.replace(pat, f'#{j}')
+        text_list = text.split(',')
+        for txt in text_list:
+            # パラメータ名, パラメータ値
+            param_name, param_value = txt.split('=')
+            param_dic = original_layer_dic[layer_name]
+            try:
+                int(param_value)
+            except:
+                param_value = param_value.replace('\'', '').replace('\"', '')
+                if '#' in param_value:
+                    index = param_value[2:-1]
+                    param_value_text = pattern_list[int(index)]
+                    param_value = tuple(map(int, param_value_text.split(',')))
+                param_dic[param_name][0] = param_value
+            else:
+                if '.' in param_value:
+                    param_value = float(param_value)
+                else:
+                    param_value = int(param_value)
+                param_dic[param_name][0] = param_value
+        temp_result[layer_name] = param_dic
+        result.append(temp_result)
+    return result
         
-
-
+    
 
 if __name__ == '__main__':
     [{'Input': {
@@ -73,3 +100,6 @@ if __name__ == '__main__':
                 'color':'AAA',
                 'detail_view':'False'
             }}]
+    
+result = convert_model_to_dict(r'C:\Users\Yuuki\Documents\GUI_MLearning\ML-Inter-Face\model_info_NN.py')
+print(result)
