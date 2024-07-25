abc389bd348f4e399233e0365e3024fbfb83fc33
B.P.S <prgrmsilver@gmail.com>
2024-07-26 06:16:31 +0900

    重みのcsvファイル化

--- packages/NNR.py
+++ packages/NNR.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
-import pandas as pd
+import math
 import csv
 
 
@@ -17,11 +17,14 @@ def Research(project_path):
     for i in range(len(model.layers)):
         deal_flag = False
         l = model.layers[i]
-        if len(l.get_weights()) == 2 and i != len(model.layers)-1:
+        layer_name = l.name
+        if len(l.get_weights()) != 0:
             raw_weights = l.get_weights()[0]
-            print(raw_weights)
-            save_csv(raw_weights)
-            break
+            save_weight = generate_save_weights(raw_weights)
+            save_csv(layer_name, save_weight, project_path)
+        else:
+            save_blank_csv(layer_name, project_path)
+        if len(l.get_weights()) == 2 and i != len(model.layers)-1:
             weights = l.get_weights()[0][:3]
             bias = l.get_weights()[1]
             deal_flag = True
@@ -29,7 +32,6 @@ def Research(project_path):
             plt.figure()
             if len(weights.shape) == 4:
                 # 重みを可視化
-                print(weights)
                 fig, axes = plt.subplots(nrows=weights.shape[0], ncols=weights.shape[1], figsize=(12, 8))
                 for j in range(weights.shape[0]):
                     for k in range(weights.shape[1]):
@@ -52,15 +54,56 @@ def Research(project_path):
             plt.savefig(f'{project_path}/weights{i}.png')
 
 
+def generate_dimensions_list(data):
+    if len(data.shape) > 2:
+        d_loop = list(data.shape)[:-2]
+        d_loop.insert(0, 1)
+        d_loop.append(1)
+        result = []
+        d_loop = d_loop[::-1]
+        for i, dim in enumerate(d_loop[1:-1]):
+            i += 1
+            inner_iter_num = math.prod(d_loop[:i-1])*d_loop[i-1]
+            outer_iter_num = math.prod(d_loop[i+1:])
+            if i == 1:
+                r = [j*inner_iter_num for j in list(range(1, dim+1))]*outer_iter_num
+            else:
+                r = np.array([[j]*inner_iter_num for j in list(range(1, dim+1))]*outer_iter_num).reshape(-1,)
+            result.append(r)
+        result = result[::-1]
+        result.append([data.shape[-2]]*len(result[0]))
+        result.append([data.shape[-1]]*len(result[0]))
+        result = list(np.array(result).T)
+    else:
+        result = list([np.array(data.shape)])
+    return result
 
+def convert_weights(data):
+    reshaped_data = list(data.reshape(-1, data.shape[-1]))
+    return reshaped_data
 
-def save_csv(weights):
-    dim = len(weights.shape)
-    for i in range(dim-2):
-        
-    with open('output.csv', 'w', newline='') as csvfile:
+def generate_save_weights(raw_weights):
+    converted_weights = convert_weights(raw_weights)
+    dim_list = generate_dimensions_list(raw_weights)
+    place_keeper = 0
+    if len(raw_weights.shape) == 1:
+        skip_iter = 1
+    else:
+        skip_iter = raw_weights.shape[-2]
+    for j, weight_line_index in enumerate(range(0, len(converted_weights), skip_iter)):
+        converted_weights.insert(weight_line_index+place_keeper, dim_list[j])
+        place_keeper += 1
+    return converted_weights
+
+def save_csv(layer_name, weights, project_path):
+    with open(f'{project_path}/{layer_name}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-    print(weights.shape)
+        for weight_line in weights:
+            writer.writerow(weight_line)
+
+def save_blank_csv(layer_name, project_path):
+    with open(f'{project_path}/{layer_name}.csv', 'w', newline='') as csvfile:
+        pass
     
 
 
