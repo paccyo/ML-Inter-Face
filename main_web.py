cee8da2a599dbdd224b274f60cee6f5456581517
BPS-sys <prgrmsilver@gmail.com>
2024-07-25 13:08:51 +0900

    web版の更新

--- main_web.py
+++ main_web.py
@@ -1,6 +1,18 @@
-from components.page_home import Home
-from components.page_project import Project
-from components.page_createproject import CreateProject
+from components.ML.page_ml_home import MLHome
+from components.ML.page_ml_createproject import MLCreateProject
+# from components.ML.page_ml_project import MLProject
+
+
+from components.ML.test_.page_ml_project import MLProject
+
+
+from components.DS.page_ds_home import DSHome
+# from components.DS.page_ds_project import DSProject
+# from components.DS.page_ds_createproject import DSNewFile
+
+from components.page_mainmenu import MainMenu
+from components.page_signin import SignIn
+from components.page_signup import SignUp
 
 import flet as ft
 
@@ -13,23 +25,34 @@ def main(page: ft.Page):
 
     def route_change(handler):
         t_route = ft.TemplateRoute(handler.route)
-        
-        if t_route.match("/Page_Home"):
+        print(t_route.route)
+        if t_route.match("/Page_SignIn"):
             page.views.clear()
-            page.views.append(Home(page))
-        elif t_route.match("/Page_Project"):
+            page.views.append(SignIn(page))
+        elif t_route.match("/Page_SignUp"):
+            page.views.append(SignUp(page))
+        elif t_route.match("/Page_MainMenu"):
             page.views.clear()
-            page.views.append(Project(page))
-        elif t_route.match("/Page_CreateProject"):
-            page.views.append(CreateProject(page))
+            page.views.append(MainMenu(page))
+        elif t_route.match("/Page_DSHome"):
+            page.views.append(DSHome(page))
+        elif t_route.match("/Page_MLHome"):
+            page.views.append(MLHome(page))
+        elif t_route.match("/Page_MLProject"):
+            page.views.clear()
+            page.views.append(MLProject(page))
+        elif t_route.match("/Page_MLCreateProject"):
+            page.views.append(MLCreateProject(page))
+        elif t_route.match("/Page_DSProject"):
+            pass
+        elif t_route.match("/Page_DSNewFile"):
+            pass
+            
         page.update()
 
     def view_pop(handler):
         page.views.pop()  # 1つ前に戻る
         page.go("/back")
-        # page.update()
-        # update() だと route が変更されない。
-        # そうなると1つ戻ってまた進むことができなくなるので go("/back") で回避。不具合？
 
     # 戻る時のロジック設定
     page.on_view_pop = view_pop
@@ -37,9 +60,12 @@ def main(page: ft.Page):
     # ルート変更時のロジック設定
     page.on_route_change = route_change
 
-    # page.go("/Page_Project")
-    page.go("/Page_Home")
+    # page.go("/Page_SignIn")
+    # page.go("/Page_DSHome")
+    page.go("/Page_MainMenu")
+    # page.go("/Page_MLHome")
+    # page.go("/Page_MLProject")
 
 # Start the app
-# ft.app(target=main)
+# ft.app(target=main, port=8550)
 ft.app(target=main, port=8550, view=ft.WEB_BROWSER)
