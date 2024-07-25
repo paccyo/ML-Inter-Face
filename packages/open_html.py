21557a05c85285b43e0d3fb00226f05ff778a7f3
B.P.S <prgrmsilver@gmail.com>
2024-07-26 06:31:00 +0900

    データ分析結果の表示機能

---
+++ packages/open_html.py
@@ -0,0 +1,15 @@
+
+import glob
+import os
+import webbrowser
+
+
+def view_DS(path):
+    path = os.path.join(path, '*.html')
+    for html_path in glob.glob(path):
+        webbrowser.open(html_path)
+
+
+
+if __name__ == '__main__':
+    view_DS(r"C:\Users\yuuki\Documents\GUI_MLearning\ML-Inter-Face\test_data\test_dir\DS_result\AutoViz")
